e6a53a5268b964352b72345075aa95ee2aca5191
Pham Huy Hieu <hieuhuy160105@gmail.com>
2025-10-13 11:58:16 +0700

    exercise for week 6

---
+++ Week 6/courseCRU_Test/src/main/java/com/example/demo/model/Course.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "courses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "course_name")
+    private String courseName;
+
+    @Column(name = "instructor")
+    private String instructor;
+
+    @Column(name = "email")
+    private String email;
+}
